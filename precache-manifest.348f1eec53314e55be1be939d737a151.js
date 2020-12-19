13b0d651e7c21ede14ecad96532841941e1a834c
Uğur Can Kök <ugurcankok0@gmail.com>
2020-12-19 23:21:38 +0300

    Update precache-manifest.348f1eec53314e55be1be939d737a151.js

--- precache-manifest.348f1eec53314e55be1be939d737a151.js
+++ precache-manifest.348f1eec53314e55be1be939d737a151.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "bfe78dbef3446e9ec34df86e474d6b8c",
-    "url": "/Blog/index.html"
+    "url": "/ugurcankok.github.io/index.html"
   },
   {
     "revision": "9c7cd2ea03f9099e4428",
-    "url": "/Blog/static/css/main.1ba91923.chunk.css"
+    "url": "/ugurcankok.github.io/static/css/main.1ba91923.chunk.css"
   },
   {
     "revision": "306d670161d3e18ef270",
-    "url": "/Blog/static/js/2.c3229690.chunk.js"
+    "url": "/ugurcankok.github.io/static/js/2.c3229690.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/Blog/static/js/2.c3229690.chunk.js.LICENSE.txt"
+    "url": "/ugurcankok.github.io/static/js/2.c3229690.chunk.js.LICENSE.txt"
   },
   {
     "revision": "9c7cd2ea03f9099e4428",
-    "url": "/Blog/static/js/main.cbcc52d4.chunk.js"
+    "url": "/ugurcankok.github.io/static/js/main.cbcc52d4.chunk.js"
   },
   {
     "revision": "b3429e7695d4558cf5fd",
-    "url": "/Blog/static/js/runtime-main.fa8b9bca.js"
+    "url": "/ugurcankok.github.io/static/js/runtime-main.fa8b9bca.js"
   },
   {
     "revision": "54597e8ba395d87b2a15b1948c658498",
-    "url": "/Blog/static/media/ai.54597e8b.jpg"
+    "url": "/ugurcankok.github.io/static/media/ai.jpg"
   },
   {
     "revision": "8363a1edb0c20a3be0ab05de593d9495",
-    "url": "/Blog/static/media/animation.8363a1ed.gif"
+    "url": "/ugurcankok.github.io/static/media/animation.gif"
   },
   {
     "revision": "e7eb9b81fb0b11d98dff63940b673934",
-    "url": "/Blog/static/media/logo.e7eb9b81.jpeg"
+    "url": "/ugurcankok.github.io/static/media/logo.jpeg"
   },
   {
     "revision": "58751424316e0f35bf8456c9e8a2aa23",
-    "url": "/Blog/static/media/machine.58751424.jpg"
+    "url": "/ugurcankok.github.io/static/media/machine.jpg"
   }
 ]);
