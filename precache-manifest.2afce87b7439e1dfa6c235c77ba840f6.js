9eb15cc03544f9f671bc284187439438803c11c0
ugurcankok <ugurcankok0@gmail.com>
2021-03-16 10:19:43 +0300

    Updates

---
+++ precache-manifest.2afce87b7439e1dfa6c235c77ba840f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ff0b7725aa8226d5e60bca3be117b8",
+    "url": "/ugurcankok.github.io/index.html"
+  },
+  {
+    "revision": "a10962fc7eeac8e454f5",
+    "url": "/ugurcankok.github.io/static/css/main.1ba91923.chunk.css"
+  },
+  {
+    "revision": "306d670161d3e18ef270",
+    "url": "/ugurcankok.github.io/static/js/2.c3229690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ugurcankok.github.io/static/js/2.c3229690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10962fc7eeac8e454f5",
+    "url": "/ugurcankok.github.io/static/js/main.34c33192.chunk.js"
+  },
+  {
+    "revision": "abadd025e299ca0bcb69",
+    "url": "/ugurcankok.github.io/static/js/runtime-main.93618e99.js"
+  },
+  {
+    "revision": "54597e8ba395d87b2a15b1948c658498",
+    "url": "/ugurcankok.github.io/static/media/ai.54597e8b.jpg"
+  },
+  {
+    "revision": "8363a1edb0c20a3be0ab05de593d9495",
+    "url": "/ugurcankok.github.io/static/media/animation.8363a1ed.gif"
+  },
+  {
+    "revision": "e7eb9b81fb0b11d98dff63940b673934",
+    "url": "/ugurcankok.github.io/static/media/logo.e7eb9b81.jpeg"
+  },
+  {
+    "revision": "58751424316e0f35bf8456c9e8a2aa23",
+    "url": "/ugurcankok.github.io/static/media/machine.58751424.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.443a02fdbc2b03bdb63f384cb1aeed20.js"
+  "/ugurcankok.github.io/precache-manifest.2afce87b7439e1dfa6c235c77ba840f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ugurcankok.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
